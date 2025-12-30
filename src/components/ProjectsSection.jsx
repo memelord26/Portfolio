0548185a52ce2dd5304ad0d13f2535488d85c262
phoebe lau <phoebelau2603@gmail.com>
2025-12-30 20:44:12 +0800

    change project images address

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -44,7 +44,7 @@ export const ProjectsSection = () => {
                     {projects.map((project, key) =>(
                         <div key={key} className="group bg-card rounded-lg overflow-hidden shadow-xs card-hover">
                             <div className="h-48 overflow-hidden">
-                                <img src={project.image} alt={project.title} className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-110" />
+                                <img src={`${import.meta.env.BASE_URL}${project.image}`} alt={project.title} className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-110" />
                             </div>
 
                             <div className="p-6">
